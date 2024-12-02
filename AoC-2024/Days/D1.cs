3368694a8af013f94741ff48d836aa175f4a2003
Krystian Bogiel <krystianbog@gmail.com>
2024-12-02 20:37:49 +0100

    Day 1 Part 1

---
+++ AoC-2024/Days/D1.cs
@@ -0,0 +1,40 @@
+﻿namespace AoC_2024.Days;
+
+internal static class D1
+{
+    internal static void Execute()
+    {
+        Console.WriteLine("D1 init");
+
+        string _inputFilePath = Path.Combine(AppContext.BaseDirectory, @"Inputs\InputD1.txt");
+        const string _inputLineSeparator = "\n";
+
+        var input = File.ReadAllText(_inputFilePath);
+        var inputList = input.Split(_inputLineSeparator).Where(x => !string.IsNullOrEmpty(x)).ToList();
+        
+        List<int> leftList = new();
+        List<int> rightList = new();
+        
+        foreach (var inputLine in inputList)
+        {
+            var inputValues = inputLine.Split("   ");
+
+            leftList.Add(int.Parse(inputValues[0]));
+            rightList.Add(int.Parse(inputValues[1]));
+        }
+
+        leftList.Sort();
+        rightList.Sort();
+
+        List<int> values = new();
+
+        for (int i = 0; i < leftList.Count; i++)
+        {
+            values.Add(Math.Abs(leftList[i] - rightList[i]));
+        }
+
+        var res = values.Sum();
+
+        Console.WriteLine($"D1 PT1: {res}");
+    }
+}
--- AoC-2024/Program.cs
+++ AoC-2024/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AoC_2024.Days;
+
+D1.Execute();
